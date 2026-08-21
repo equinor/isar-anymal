c072b6b5f47a86ca6a0a9fd14f164897dcb1859e
Christian de Jonge <chjo@equinor.com>
2026-08-21 11:57:19 +0200

    Restore package logging and log discarded gas-monitoring events

--- src/isar_anymal/robot/api/anymal_api/server_sent_event_handlers/inspection_handler.py
+++ src/isar_anymal/robot/api/anymal_api/server_sent_event_handlers/inspection_handler.py
@@ -99,8 +99,22 @@ def _process_inspection_event(
     interpreted data is available in the event, but not used.
     """
 
+    logger.info(
+        f"Received inspection event: measurement type={event.measurement.type}, "
+        f"asset ID={event.asset_id}, task run UID={event.task_run_uid}, "
+        f"mission run ID={event.metadata.mission_run_id}"
+    )
+
     if event.measurement.type == InspectionMeasurementType.IMT_CONCENTRATION_MONITORING:
+        # Discarded, as before. Logged so we can see which measurement type
+        # CO2 tasks actually emit; IMT_CONCENTRATION is handled below.
+        logger.warning(
+            f"Discarding IMT_CONCENTRATION_MONITORING inspection event for asset "
+            f"ID {event.asset_id} (task run UID {event.task_run_uid}). No inspection "
+            f"will be created or uploaded for this event."
+        )
         return
+
     logger.info(event)
 
     if event.measurement.sensor_pose.frame_id == "invalid":
